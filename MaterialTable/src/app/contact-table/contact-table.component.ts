27fa30ac1f3dd3a930db636631397fa51838aea6
kemora08 <kevin_mora89@hotmail.com>
2020-08-28 11:55:33 -0500

    initial commit

--- MaterialTable/src/app/contact-table/contact-table.component.ts
+++ MaterialTable/src/app/contact-table/contact-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../Services/contact.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { IContact } from '../Interface/IContact';
 
 @Component({
   selector: 'app-contact-table',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-table.component.css']
 })
 export class ContactTableComponent implements OnInit {
-
-  constructor() { }
+  dataSource: MatTableDataSource<IContact>;
+  displayedColumns: string [] = ['FirstName', 'LastName', 'Address', 'PhoneNumber'];
+  constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.contactService.getContacts());
   }
 
 }
